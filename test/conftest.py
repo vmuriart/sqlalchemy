1c768e556a6e0b8efbb7732bc581a16c3d1b4b44
Victor Uriarte <victor.m.uriarte@intel.com>
2016-09-11 20:20:30 -0700

    fa

--- test/conftest.py
+++ test/conftest.py
@@ -18,14 +18,3 @@ if not sys.flags.no_user_site:
     sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)),
                                     '..', 'lib'))
 
-# use bootstrapping so that test plugins are loaded
-# without touching the main library before coverage starts
-bootstrap_file = os.path.join(
-    os.path.dirname(__file__), "..", "lib", "sqlalchemy", "testing",
-    "plugin", "bootstrap.py")
-
-with open(bootstrap_file) as f:
-    code = compile(f.read(), "bootstrap.py", 'exec')
-    to_bootstrap = "pytest"
-    exec (code, globals(), locals())
-    from pytestplugin import *  # noqa
